dfbfd03d35791e01c4d8dd008720d5e56f618ae5
Shashuvo <shahariat2810@gmail.com>
2026-02-08 02:03:46 +0600

    banner layout setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import coxs from "../../assets/images/Rectangle 1.png"
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home</h1>
+        <div className="flex items-center justify-center gap-11 w-full h-screen bg-cover bg-center"
+            style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url(${coxs})` }}>
+                {/* left banner */}
+                <div>
+                    <h1>title</h1>
+                </div>
+                {/* right banner */}
+                <div>
+                    <h1>pic</h1>
+                </div>
         </div>
     );
 };
