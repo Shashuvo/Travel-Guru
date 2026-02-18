1f931c875bb420999933a06c2c9d2c70f3b66694
Shashuvo <shahariat2810@gmail.com>
2026-02-18 18:46:46 +0600

    background image dynamically added

---
+++ src/assets/images.js
@@ -0,0 +1,17 @@
+import cox from "./images/Rectangle 1.png"
+import sajek from "./images/Sajek.png"
+import sundarbans from "./images/sundorbon.png"
+import sreemangal from "./images/Sreemongol.png"
+import saintMartin from "./images/saint-martin.png"
+import kuakata from "./images/kuakata.png"
+import rangamati from "./images/rangamati.png"
+
+export const images = {
+    1: cox,
+    2: sajek,
+    3: sundarbans,
+    4: sreemangal,
+    5: saintMartin,
+    6: kuakata,
+    7: rangamati
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-import coxs from "../../assets/images/Rectangle 1.png"
 import HomeBanner from '../../components/HomeBanner';
 import { useLoaderData } from 'react-router';
+import { images } from '../../assets/images';
 
 const Home = () => {
     const places = useLoaderData();
     const [selectedPlace, setSelectedPlace] = useState(places[0]);
     console.log(places);
-    console.log(places.length);
+    console.log(selectedPlace);
 
     return (
         <div className="flex items-center justify-center w-full h-screen bg-cover bg-center"
-            style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url(${coxs})` }}>
+            style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url(${images[selectedPlace.id]})` }}>
             <div className='w-full md:w-10/12'>
                 {/*banner */}
                 <HomeBanner place={selectedPlace} places={places} setSelectedPlace={setSelectedPlace}></HomeBanner>
